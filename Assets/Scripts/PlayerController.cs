cc73967101e571711d36b3c5d0a9c9e62120e38d
OrangeBurrito <mali.kewl.pool@gmail.com>
2022-05-09 00:33:09 +0700

    Add basic (very wonky) movement with OnMove

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,15 +6,18 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour {
 	private Rigidbody2D rb;
 	public float moveSpeed = 1f;
+	private float inputX;
 
 	void Awake() {
 		rb = GetComponent<Rigidbody2D>();
 	}
 
-	void FixedUpdate() {
+	void Update() {
+		rb.velocity = new Vector2(inputX * moveSpeed, rb.velocity.y);
 	}
 
 	public void OnMove(InputValue input) {
+		inputX = input.Get<Vector2>().x;
 	}
 
 	public void OnJump() {
